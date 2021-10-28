27b89c69d24b1b1bf68785aac9950a6d626a93bb
Gustavo Oliveira <gustavo.gabriel@beresolution.com>
2021-10-28 17:48:47 -0300

    Finalizando site

--- control.js
+++ control.js
@@ -1,35 +1,19 @@
-const btnClose = document.getElementById('close-carta');
+const btnOpenLetter = document.getElementById('open-carta');
+const btnCloseLetter = document.getElementById('close-carta');
 
-const contentCarta = document.querySelectorAll('.content')[0];
-const contentGallery = document.querySelectorAll('.gallery')[0];
 
 const btnOpenGallery = document.getElementById('open-gallery')
 const btnCloseGallery = document.getElementById('close-gallery');
 
-function mudaStyle() {
-  btnClose.style.marginTop = '16px';
-}
+const contentLetter = document.querySelectorAll('.content')[0];
+const contentGallery = document.querySelectorAll('.gallery')[0];
 
-function resetaStyle() {
-  btnClose.style.marginTop = 'inherent';
-}
+btnOpenLetter.addEventListener('click', () => {
+  contentLetter.style.display = 'block'
+})
 
-function ToggleBtn() {
-  if (contentCarta.style.display === 'none'){
-    contentCarta.style.display = 'block';
-  
-    mudaStyle();
-
-    btnClose.innerHTML = 'Fechar Carta'
-    return;
-  }
-  contentCarta.style.display = 'none';
-  btnClose.innerHTML = 'Abrir Carta'
-  resetaStyle();
-}
-
-btnClose.addEventListener('click', (e) => {
-  ToggleBtn();
+btnCloseLetter.addEventListener('click', () => {
+  contentLetter.style.display = 'none'
 })
 
 btnOpenGallery.addEventListener('click', () => {
